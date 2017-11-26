8ef8d1ab0a98c3a80032921993c3dedbcf69bb5b
Eloi Carracedo <eloimuns@gmail.com>
2017-11-26 01:57:17 +0100

    Add methods needed for CORS

--- middlewares/middlewareCors.js
+++ middlewares/middlewareCors.js
@@ -7,7 +7,8 @@
 // Function to inject the timestamp into the requests
 var corsMiddleware = function (req, res, next) {
     res.header("Access-Control-Allow-Origin", "*");
-    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept, token, userData");
+	res.header("Access-Control-Allow-Methods", "OPTIONS, GET, POST, PUT, DELETE");
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
     next();
 };
 
